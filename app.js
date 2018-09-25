0e3f8547c57345544d333b977ecc41aa32c56fdf
anujm4467 <anujm4467@gmail.com>
2018-09-25 12:04:04 +0530

    changing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = require('express');
 app.set(morgan('dev'));
 
 app.get('/', (req, res)=>{
-    res.send('hi its me..');
+    res.send({'hi' :'there'});
 })
 
 const PORT =  process.env.port || 3333;
