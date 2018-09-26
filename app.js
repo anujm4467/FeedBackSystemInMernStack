b36746a59cf49529fe739cbee1bb9844a07c0254
anujm4467 <anujm4467@gmail.com>
2018-09-26 10:19:16 +0530

    port c

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 
-
 const app = express.Router();
 
 //app.set(morgan('dev'));
@@ -10,7 +9,7 @@ app.get('/', (req, res)=>{
     res.send({'hi' :'there'});
 })
 
-const PORT =  process.env.port || 5555;
+const PORT =  process.env.PORT || 5555;
 app.listen(PORT, (err)=>{
     if(err) return err;
     console.log('listning to '+ PORT);
